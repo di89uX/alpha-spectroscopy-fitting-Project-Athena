14d76b67535ff2bdab40b6cb893c7a4c02302b83
Dineth Senuja Wijesinghe <dineth228s@gmail.com>
2026-01-03 06:44:15 +0530

    Tested with real Eu-152 experimental data

--- GammaSpectraFitting/run_Fit_multiPeaks_Alef.py
+++ GammaSpectraFitting/run_Fit_multiPeaks_Alef.py
@@ -29,7 +29,7 @@ except ImportError:
         return {'error': 'full_multi_peak_analysis function is not implemented or imported.'}
 
 # --- Configuration ---
-FILE_PATH = r'GammaSpectraFitting\data\raw\synthetic\Cs137_spectrum.csv'
+FILE_PATH = r'GammaSpectraFitting\data\raw\real\Eu-152.csv'
 BACKGROUND_PARAMS_COUNT = 3 # (c0, c1, c2)
 
 # Function to format a measurement as Value ± Uncertainty
